8f23b6a6e3bcb7791aced13e9b42e6102af499f4
Shreyash Vishwasrao <shreyashv0929@gmail.com>
2024-06-17 19:29:13 +0530

    Completed navigation bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import NavBar from "./components/NavBar";
+// import "/src/App.css";
+
 function App() {
   return (
     <>
-      <div></div>
+      <div id="instructors" className="h-screen w-full">
+        <img src="/src/assets/Background.png" alt="" className="" />
+        <NavBar />
+      </div>
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import NavItems from "./NavItems";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="flex justify-around items-center px-10 fixed top-0 left-0 right-0 border-b border-black">
+      <section className="brand-logo py-2">
+        <img src="src/assets/logo.png" alt="" className="" />
+        {/* Brand Logo */}
+      </section>
+      <section className="w-9/12">
+        <ul className="flex justify-center p-2">
+          <NavItems itemName="Home" target="#" />
+          <NavItems itemName="Internships" target="#" />
+          <NavItems itemName="Mock Tests" target="#" />
+          <NavItems itemName="Learning" target="#" />
+          <NavItems itemName="About" target="#" />
+        </ul>
+      </section>
+      <section className="">
+        <button className="bg-blue-900 px-4 py-2 rounded-full text-white text-lg">
+          Get Started
+        </button>
+      </section>
+    </div>
+  );
+}
+
+export default NavBar;
